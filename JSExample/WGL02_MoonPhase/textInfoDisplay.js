e14f724dcb354d120ee2d402aa9eec54e6bcdab0
dknife <ymkang@San302-YMKang.local>
2015-08-13 15:55:17 +0900

    update 0812

--- JSExample/WGL02_MoonPhase/textInfoDisplay.js
+++ JSExample/WGL02_MoonPhase/textInfoDisplay.js
@@ -2,7 +2,9 @@ var today = new Date();
 
 var phase = moonPhase();
 
-document.write("<ul><li>오늘은 " + today.getFullYear() +"년 "+(today.getMonth()+1)+"월 "+today.getDate()+"일</li>");
+var monthNum = today.getMonth();
+
+document.write("<ul><li>Today is "  +(today.getMonth()+1)+", " +today.getDate()+ "," + today.getFullYear() + "</li>");
 document.write('<li>달님 상태는 <b><span id="moonPhase">'+phase+'</span></b></li>');
 document.write('<li>영민 - JavaScript+WebGL 연습</li></ul>');
 	
