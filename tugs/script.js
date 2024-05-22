4069d9201875d61973c3077f514ef224c94360cc
Dknife <young.min.kang@gmail.com>
2024-05-22 11:29:29 +0900

    Update script.js

--- tugs/script.js
+++ tugs/script.js
@@ -33,7 +33,7 @@ function flipCard() {
 
 	//this.textContent = this.dataset.value;
 	let value = this.dataset.value;
-	let imgTag = `<img src=./${value}.jpg width=150>`;
+	let imgTag = `<img src=./${value}.jpg width=100>`;
 	this.innerHTML = imgTag;
 	
 	this.classList.add('flipped');
